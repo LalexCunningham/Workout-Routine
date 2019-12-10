842e0274fd8b6d46044b9490a0369d72cbf860c7
Randomer003 <lalexcunningham@gmail.com>
2019-12-10 13:27:40 -0800

    Add external files

--- dataGenerator.py
+++ dataGenerator.py
@@ -1,1 +1,22 @@
-import datetime as dt
+from datetime import datetime as dt
+import csv
+import pandas as pd
+
+exercises = ['Pushups', 'Pullups', 'Planks', 'Squats']
+
+def initializeCSV(date, columns, timeframe):
+    cols = ['date']
+    for exercise in columns:
+        cols.append(exercise)
+    df = pd.DataFrame
+    df.columns = cols
+    '''
+    datelist = pd.date_range(date, periods=timeframe).to_list()
+    datesColumn = []
+    for day in datelist:
+        datesColumn.append(day.strftime('%d.%m.%Y'))
+    df.date = datesColumn
+    '''
+    df.to_csv(r'./Workout-data/test.csv')
+
+initializeCSV(dt.today(), exercises, 100)
